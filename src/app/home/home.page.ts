a998f383b895f04642319868e74d8dc0e33e8ff6
David-Rodriguez-cabrera <alum.david.rodriguez.cabrera@ieslosremedios.org>
2021-10-29 12:50:13 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  num: number;
+  numSecret: number = this.numAleatorio(1, 100);
+  mayorMenor: string = '...';
+  constructor() {
+    console.log("El número secreto es:" + this.numSecret)
+  }
+  numAleatorio(a, b){
+    return Math.round(Math.random()*(b-a)+parseInt(a))
+  }
+  compruebaNumero(){
+    if(this.num){
+      if(this.numSecret < this.num){
+        this.mayorMenor = 'menor';
+      } else if(this.numSecret > this.num){
+        this.mayorMenor = "mayor";
+      }else{
+        this.mayorMenor = "igual";
+      }
+    }
+  }
+  reinicia(){
+    // Reiniciamos las variables
+    this.num = null;
+    this.mayorMenor = '...';
+    this.numSecret = this.numAleatorio(1, 100);
+    console.log("El número secreto es:" + this.numSecret);
+  }
 }
